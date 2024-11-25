8c192c763349f776fde2405e50b4c84ecca52fc1
Antonio Cruz <antonio.cruz5612@gmail.com>
2024-11-24 22:40:35 -0800

    create a solution that will check if the number leaves a remainder when divided by 2. Depending on the output of this condition, the numbger will be multiplied by either 8 or 9.

--- simple-multiplication.py
+++ simple-multiplication.py
@@ -1,6 +1,11 @@
 # This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
 
+my_number = 7
+
 def simple_multiplication(number):
-    print("Hello World!")
+    if number % 2 != 0:
+        print(number * 8)
+    else: 
+        print(number * 9)
     
-simple_multiplication()
+simple_multiplication(my_number)
