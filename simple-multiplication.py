400f25cda3027a933b9c1abb26262dd4e2546d55
Antonio Cruz <antonio.cruz5612@gmail.com>
2024-11-24 22:34:43 -0800

    create a function that will satisfy the instructions of practice problem.

--- simple-multiplication.py
+++ simple-multiplication.py
@@ -1,1 +1,6 @@
-print("Hello World!")
+# This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+def simple_multiplication(number):
+    print("Hello World!")
+    
+simple_multiplication()
